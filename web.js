3e474afef8341967cd6c93b63ce50eb317c7731b
paoloceravolo <paolo.ceravolo@unimi.it>
2015-04-21 15:43:12 +0200

    variables

--- web.js
+++ web.js
@@ -10,4 +10,11 @@ function processa(req, res){
 	}
 
 var s = http.createServer(processa);
-s.listen(80);
+s.listen(8080);
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", server_port " + port )
+});
