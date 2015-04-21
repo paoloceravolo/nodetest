8afef3bf28e38d02f5b0be5eeef2863bf6ad8085
paoloceravolo <paolo.ceravolo@unimi.it>
2015-04-21 15:56:19 +0200

    listen

--- web.js
+++ web.js
@@ -9,12 +9,8 @@ function processa(req, res){
 	
 	}
 
-var s = http.createServer(processa);
-s.listen(8080);
+var port =  process.env.OPENSHIFT_NODEJS_PORT || 8080;   // Port 8080 if you run locally
+var address =  process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1"; // Listening to localhost if you run locally
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
- 
-server.listen(server_port, server_ip_address, function () {
-  console.log( "Listening on " + server_ip_address + ", server_port " + port )
-});
+var s = http.createServer(processa);
+s.listen(port, address);
