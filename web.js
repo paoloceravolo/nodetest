0e5a53a63fa944025e39824e5851866d48ec5a8a
paoloceravolo <paolo.ceravolo@unimi.it>
2015-05-20 11:22:55 +0200

    paolo

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var http = require("http");
 
 function processa(req, res){
-	var corpo = 'Sono qui! mi hai chimato!\n';
+	var corpo = 'Sono qui! mi hai chimato da ' + req.url + ' con metodo: ' + req.method + '\n';
 	var content_length = corpo.length;
 	
 	res.writeHead(200, {'Content-Length': content_length, 'Content-Type': 'text/plain'});
