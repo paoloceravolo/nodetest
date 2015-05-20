d739e33e3d1d55dde6ca8dce40d41f76e422f371
paoloceravolo <paolo.ceravolo@unimi.it>
2015-05-20 22:09:29 +0200

    serve files

--- web.js
+++ web.js
@@ -1,13 +1,52 @@
-var http = require("http");
+var http = require('http'),
+    url = require('url'),
+    path = require('path'),
+    fs = require('fs');
+var mimeTypes = {
+    "html": "text/html",
+    "jpeg": "image/jpeg",
+    "jpg": "image/jpeg",
+    "png": "image/png",
+    "js": "text/javascript",
+    "css": "text/css"};
 
-function processa(req, res){
-	var corpo = 'Sono qui! mi hai chimato da ' + req.url + ' con metodo: ' + req.method + '\n';
-	var content_length = corpo.length;
-	
-	res.writeHead(200, {'Content-Length': content_length, 'Content-Type': 'text/plain'});
-	res.end(corpo);
-	
-	}
+function processa(req, res) {
+  var uri = url.parse(req.url).pathname;
+  var filename = path.join(process.cwd(), unescape(uri));
+  var stats;
+
+  try {
+    stats = fs.lstatSync(filename); // throws if path doesn't exist
+  } catch (e) {
+    res.writeHead(404, {'Content-Type': 'text/plain'});
+    res.write('404 Not Found\n');
+    res.end();
+    return;
+  }
+
+
+  if (stats.isFile()) {
+    // path exists, is a file
+    var mimeType = mimeTypes[path.extname(filename).split(".").reverse()[0]];
+    res.writeHead(200, {'Content-Type': mimeType} );
+
+    var fileStream = fs.createReadStream(filename);
+    fileStream.pipe(res);
+  } else if (stats.isDirectory()) {
+    // path exists, is a directory
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write('Index of '+uri+'\n');
+    res.write('TODO, show index?\n');
+    res.end();
+  } else {
+    // Symbolic link, other?
+    // TODO: follow symlinks?  security?
+    res.writeHead(500, {'Content-Type': 'text/plain'});
+    res.write('500 Internal server error\n');
+    res.end();
+  }
+
+}
 
 var port =  process.env.OPENSHIFT_NODEJS_PORT || 8080;   // Port 8080 if you run locally
 var address =  process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1"; // Listening to localhost if you run locally
