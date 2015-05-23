b0ec0c5df2c7972c4a99a02af50272f655fc0da9
paoloceravolo <paolo.ceravolo@unimi.it>
2015-05-23 13:21:30 +0200

    express

--- web.js
+++ web.js
@@ -4,8 +4,10 @@ var app=express();
 var port =  process.env.OPENSHIFT_NODEJS_PORT || 8080;   // Port 8080 if you run locally
 var address =  process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1"; // Listening to localhost if you run locally
 
+app.engine('.html', require('ejs').renderFile);
+
 app.get('/',function(req,res){
-res.render('index.html')
+res.render('index')
 });
 app.get('/about',function(req,res){
 res.render('about.html');
