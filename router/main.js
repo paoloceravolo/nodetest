366951ec6366dfad2c1715545f2b585580875538
paoloceravolo <paolo.ceravolo@unimi.it>
2015-05-28 18:26:19 +0200

    test

---
+++ router/main.js
@@ -0,0 +1,15 @@
+// inclusione di app da web.js
+module.exports=function(app)
+{
+
+// logica di gestione delle richieste	
+app.get('/',function(req,res){
+res.render('index.html')
+});
+app.get('/about',function(req,res){
+res.render('about.html')
+});
+app.get('/estemplate',function(req,res){
+res.render('estemplate', { title: 'Prova Templating' });
+});
+}
--- web.js
+++ web.js
@@ -1,16 +1,13 @@
-var http = require("http");
-
-function processa(req, res){
-	var corpo = 'Sono qui! mi hai chimato da ' + req.url + ' con metodo: ' + req.method + '\n';
-	var content_length = corpo.length;
-	
-	res.writeHead(200, {'Content-Length': content_length, 'Content-Type': 'text/plain'});
-	res.end(corpo);
-	
-	}
+var express=require('express');
+var app=express();
 
 var port =  process.env.OPENSHIFT_NODEJS_PORT || 8080;   // Port 8080 if you run locally
 var address =  process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1"; // Listening to localhost if you run locally
 
-var s = http.createServer(processa);
-s.listen(port, address);
+// attivazione di router e di EJS per la gestione del rendering
+require('./router/main')(app);
+app.set('views',__dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('.html', require('ejs').renderFile);
+
+var server=app.listen(port, address);
