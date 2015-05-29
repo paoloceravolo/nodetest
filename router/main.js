32f2784897e49d88aefe8df5941eec2d84c2a054
paoloceravolo <paolo.ceravolo@unimi.it>
2015-05-29 13:04:12 +0200

    Mongo DB

--- router/main.js
+++ router/main.js
@@ -1,15 +1,225 @@
-// inclusione di app da web.js
-module.exports=function(app)
-{
+// Set up our DB API globals.
+var Db         = require('mongodb').Db;
+var Connection = require('mongodb').Connection;
+var Server     = require('mongodb').Server;
+var BSON       = require('mongodb').BSON;
+var ObjectID   = require('mongodb').ObjectID;
 
-// logica di gestione delle richieste	
-app.get('/',function(req,res){
-res.render('index.html')
+// Main DB provider object
+
+var server = new Server(host, port, { auto_reconnect: true }, {});
+db = new Db(process.env.OPENSHIFT_APP_NAME, server);
+ 
+//var server = new Server('localhost', 27017, {auto_reconnect: true});
+//db = new Db('winedb', server);
+ 
+db.open(function(err, db) {
+    if(!err) {
+        console.log("Connected to 'winedb' database");
+        db.collection('partite', {strict:true}, function(err, collection) {
+            if (err) {
+                console.log("The 'partite' collection doesn't exist. Creating it with sample data...");
+                populateDB();
+            }
+        });
+    }
 });
-app.get('/about',function(req,res){
-res.render('about.html')
-});
-app.get('/estemplate',function(req,res){
-res.render('estemplate', { title: 'Prova Templating' });
-});
+ 
+exports.findById = function(req, res) {
+    var id = req.params.id;
+    console.log('Retrieving wine: ' + id);
+    db.collection('partite', function(err, collection) {
+        collection.findOne({'_id':new BSON.ObjectID(id)}, function(err, item) {
+            res.send(item);
+        });
+    });
+};
+ 
+exports.findAll = function(req, res) {
+    db.collection('partite', function(err, collection) {
+        collection.find().toArray(function(err, items) {
+            res.send(items);
+        });
+    });
+};
+ 
+exports.addWine = function(req, res) {
+    var partita = req.body;
+    console.log('Adding wine: ' + JSON.stringify(partita));
+    db.collection('partite', function(err, collection) {
+        collection.insert(partita, {safe:true}, function(err, result) {
+            if (err) {
+                res.send({'error':'An error has occurred'});
+            } else {
+                console.log('Success: ' + JSON.stringify(result[0]));
+                res.send(result[0]);
+            }
+        });
+    });
 }
+ 
+exports.updateWine = function(req, res) {
+    var id = req.params.id;
+    var partita = req.body;
+    console.log('Updating wine: ' + id);
+    console.log(JSON.stringify(partita));
+    db.collection('partite', function(err, collection) {
+        collection.update({'_id':new BSON.ObjectID(id)}, partita, {safe:true}, function(err, result) {
+            if (err) {
+                console.log('Error updating wine: ' + err);
+                res.send({'error':'An error has occurred'});
+            } else {
+                console.log('' + result + ' document(s) updated');
+                res.send(partita);
+            }
+        });
+    });
+}
+ 
+exports.deleteWine = function(req, res) {
+    var id = req.params.id;
+    console.log('Deleting wine: ' + id);
+    db.collection('partite', function(err, collection) {
+        collection.remove({'_id':new BSON.ObjectID(id)}, {safe:true}, function(err, result) {
+            if (err) {
+                res.send({'error':'An error has occurred - ' + err});
+            } else {
+                console.log('' + result + ' document(s) deleted');
+                res.send(req.body);
+            }
+        });
+    });
+}
+ 
+/*--------------------------------------------------------------------------------------------------------------------*/
+// Populate database with sample data -- Only used once: the first time the application is started.
+// You'd typically not find this code in a real-life app, since the database would already exist.
+var populateDB = function() {
+ 
+    var partite = [
+    {
+                "giornata": "Prima Giornata",
+                "casa": "Galles",
+                "ospite": "Inghilterra",
+                "ptCasa": "16",
+                "ptOspite": "21",
+                "stadio": "Millennium Stadium"
+            },
+            {
+                "giornata": "Prima Giornatat",
+                "casa": "Italia",
+                "ospite": "Irlanda",
+                "ptCasa": "3",
+                "ptOspite": "26",
+                "stadio": "Stadio Olimpico"
+            },
+            {
+                "giornata": "Prima Giornata",
+                "casa": "Francia",
+                "ospite": "Scozia",
+                "ptCasa": "15",
+                "ptOspite": "8",
+                "stadio": "Stade de France"
+            },
+            {
+                "giornata": "Seconda Giornata",
+                "casa": "Inghilterra",
+                "ospite": "Italia",
+                "ptCasa": "47",
+                "ptOspite": "17",
+                "stadio": "Twickenham"
+            },
+            {
+                "giornata": "Seconda Giornata",
+                "casa": "Irlanda",
+                "ospite": "Francia",
+                "ptCasa": "18",
+                "ptOspite": "11",
+                "stadio": "Aviva Stadium"
+            },
+            {
+                "giornata": "Seconda Giornata",
+                "casa": "Scozia",
+                "ospite": "Galles",
+                "ptCasa": "23",
+                "ptOspite": "26",
+                "stadio": "Murrayfield"
+            },
+            {
+                "giornata": "Terza Giornata",
+                "casa": "Scozia",
+                "ospite": "Italia",
+                "ptCasa": "19",
+                "ptOspite": "22",
+                "stadio": "Murrayfield"
+            },
+            {
+                "giornata": "Terza Giornata",
+                "casa": "Francia",
+                "ospite": "Galles",
+                "ptCasa": "13",
+                "ptOspite": "20",
+                "stadio": "Stade de France"
+            },
+            {
+                "giornata": "Terza Giornata",
+                "casa": "Irlanda",
+                "ospite": "Inghilterra",
+                "ptCasa": "19",
+                "ptOspite": "9",
+                "stadio": "Aviva Stadium"
+            },
+            {
+                "giornata": "Quarta Giornata",
+                "casa": "Galles",
+                "ospite": "Irlanda",
+                "ptCasa": "23",
+                "ptOspite": "16",
+                "stadio": "Millennium Stadium"
+            },
+            {
+                "giornata": "Quarta Giornata",
+                "casa": "Inghilterra",
+                "ospite": "Scozia",
+                "ptCasa": "25",
+                "ptOspite": "13",
+                "stadio": "Twickenham"
+            },
+            {
+                "giornata": "Quarta Giornata",
+                "casa": "Italia",
+                "ospite": "Francia",
+                "ptCasa": "0",
+                "ptOspite": "29",
+                "stadio": "Stadio Olimpico"
+            },
+            {
+                "giornata": "Quinta Giornata",
+                "casa": "Italia",
+                "ospite": "Galles",
+                "ptCasa": "20",
+                "ptOspite": "61",
+                "stadio": "Stadio Olimpico"
+            },
+            {
+                "giornata": "Quinta Giornata",
+                "casa": "Scozia",
+                "ospite": "Irlanda",
+                "ptCasa": "10",
+                "ptOspite": "40",
+                "stadio": "Murrayfield"
+            },
+            {
+                "giornata": "Quinta Giornata",
+                "casa": "Inghilterra",
+                "ospite": "Francia",
+                "ptCasa": "55",
+                "ptOspite": "35",
+                "stadio": "Twickenham"
+            }];
+ 
+    db.collection('partite', function(err, collection) {
+        collection.insert(partite, {safe:true}, function(err, result) {});
+    });
+ 
+};
--- web.js
+++ web.js
@@ -1,13 +1,20 @@
-var express=require('express');
-var app=express();
+var express = require('express');
+var partite = require('./router/main');
+var app = express();
 
+app.configure(function () {
+    app.use(express.logger('dev'));  
+    app.use(express.bodyParser());
+});
+
+app.get('/partite', partite.findAll);
+app.get('/partite/:id', partite.findById);
+app.post('/partite', partite.addWine);
+app.put('/partite/:id', partite.updateWine);
+app.delete('/partite/:id', partite.deleteWine);
+ 
 var port =  process.env.OPENSHIFT_NODEJS_PORT || 8080;   // Port 8080 if you run locally
 var address =  process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1"; // Listening to localhost if you run locally
 
-// attivazione di router e di EJS per la gestione del rendering
-require('./router/main')(app);
-app.set('views',__dirname + '/views');
-app.set('view engine', 'ejs');
-app.engine('.html', require('ejs').renderFile);
 
 var server=app.listen(port, address);
