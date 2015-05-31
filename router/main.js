39711a5d1a9797466575f3863e5425ebb14f9405
paoloceravolo <paolo.ceravolo@unimi.it>
2015-05-31 19:39:59 +0200

    mongo

---
+++ router/main.js
@@ -0,0 +1,226 @@
+// Set up our DB API globals.
+var Db         = require('mongodb').Db;
+var Connection = require('mongodb').Connection;
+var Server     = require('mongodb').Server;
+var BSON       = require('mongodb').BSON;
+var ObjectID   = require('mongodb').ObjectID;
+
+
+// Main DB provider object
+PProvider = function(host, port, user, pass) {
+    this.db = new Db(process.env.OPENSHIFT_APP_NAME, new Server(host, port, { auto_reconnect: true }, {}));
+    this.db.open(function(error, db){
+        db.authenticate(user, pass, function(error, result) {
+
+			if(!err) {
+        console.log("Connesso al database");
+        	db.collection('partite', {strict:true}, function(err, collection) {
+            if (err) {
+                console.log("The 'partite' collection doesn't exist. Creating it with sample data...");
+                populateDB();
+            }
+        	});
+		}
+    });
+};
+};
+ 
+PProvider.prototype.findById = function(req, res) {
+    var id = req.params.id;
+    console.log('Retrieving data: ' + id);
+    db.collection('partite', function(err, collection) {
+        collection.findOne({'_id':new BSON.ObjectID(id)}, function(err, item) {
+            res.send(item);
+        });
+    });
+};
+ 
+PProvider.prototype.findAll = function(req, res) {
+    db.collection('partite', function(err, collection) {
+        collection.find().toArray(function(err, items) {
+            res.send(items);
+        });
+    });
+};
+ 
+PProvider.prototype.addP = function(req, res) {
+    var partita = req.body;
+    console.log('Adding data: ' + JSON.stringify(partita));
+    db.collection('partite', function(err, collection) {
+        collection.insert(partita, {safe:true}, function(err, result) {
+            if (err) {
+                res.send({'error':'Sorry something went wrong'});
+            } else {
+                console.log('Success: ' + JSON.stringify(result[0]));
+                res.send(result[0]);
+            }
+        });
+    });
+}
+ 
+PProvider.prototype.updateP = function(req, res) {
+    var id = req.params.id;
+    var partita = req.body;
+    console.log('Updating data: ' + id);
+    console.log(JSON.stringify(partita));
+    db.collection('partite', function(err, collection) {
+        collection.update({'_id':new BSON.ObjectID(id)}, partita, {safe:true}, function(err, result) {
+            if (err) {
+                console.log('Error updating partite: ' + err);
+                res.send({'error':'Sorry something went wrong'});
+            } else {
+                console.log('' + result + ' document(s) updated');
+                res.send(partita);
+            }
+        });
+    });
+}
+ 
+PProvider.prototype.deleteP = function(req, res) {
+    var id = req.params.id;
+    console.log('Deleting data: ' + id);
+    db.collection('partite', function(err, collection) {
+        collection.remove({'_id':new BSON.ObjectID(id)}, {safe:true}, function(err, result) {
+            if (err) {
+                res.send({'error':' Sorry something went wrong ' + err});
+            } else {
+                console.log('' + result + ' document(s) deleted');
+                res.send(req.body);
+            }
+        });
+    });
+}
+ 
+/*--------------------------------------------------------------------------------------------------------------------*/
+// Populate database with sample data -- Only used once: the first time the application is started.
+// You'd typically not find this code in a real-life app, since the database would already exist.
+var populateDB = function() {
+ 
+    var partite = [
+    {
+                "giornata": "Prima Giornata",
+                "casa": "Galles",
+                "ospite": "Inghilterra",
+                "ptCasa": "16",
+                "ptOspite": "21",
+                "stadio": "Millennium Stadium"
+            },
+            {
+                "giornata": "Prima Giornatat",
+                "casa": "Italia",
+                "ospite": "Irlanda",
+                "ptCasa": "3",
+                "ptOspite": "26",
+                "stadio": "Stadio Olimpico"
+            },
+            {
+                "giornata": "Prima Giornata",
+                "casa": "Francia",
+                "ospite": "Scozia",
+                "ptCasa": "15",
+                "ptOspite": "8",
+                "stadio": "Stade de France"
+            },
+            {
+                "giornata": "Seconda Giornata",
+                "casa": "Inghilterra",
+                "ospite": "Italia",
+                "ptCasa": "47",
+                "ptOspite": "17",
+                "stadio": "Twickenham"
+            },
+            {
+                "giornata": "Seconda Giornata",
+                "casa": "Irlanda",
+                "ospite": "Francia",
+                "ptCasa": "18",
+                "ptOspite": "11",
+                "stadio": "Aviva Stadium"
+            },
+            {
+                "giornata": "Seconda Giornata",
+                "casa": "Scozia",
+                "ospite": "Galles",
+                "ptCasa": "23",
+                "ptOspite": "26",
+                "stadio": "Murrayfield"
+            },
+            {
+                "giornata": "Terza Giornata",
+                "casa": "Scozia",
+                "ospite": "Italia",
+                "ptCasa": "19",
+                "ptOspite": "22",
+                "stadio": "Murrayfield"
+            },
+            {
+                "giornata": "Terza Giornata",
+                "casa": "Francia",
+                "ospite": "Galles",
+                "ptCasa": "13",
+                "ptOspite": "20",
+                "stadio": "Stade de France"
+            },
+            {
+                "giornata": "Terza Giornata",
+                "casa": "Irlanda",
+                "ospite": "Inghilterra",
+                "ptCasa": "19",
+                "ptOspite": "9",
+                "stadio": "Aviva Stadium"
+            },
+            {
+                "giornata": "Quarta Giornata",
+                "casa": "Galles",
+                "ospite": "Irlanda",
+                "ptCasa": "23",
+                "ptOspite": "16",
+                "stadio": "Millennium Stadium"
+            },
+            {
+                "giornata": "Quarta Giornata",
+                "casa": "Inghilterra",
+                "ospite": "Scozia",
+                "ptCasa": "25",
+                "ptOspite": "13",
+                "stadio": "Twickenham"
+            },
+            {
+                "giornata": "Quarta Giornata",
+                "casa": "Italia",
+                "ospite": "Francia",
+                "ptCasa": "0",
+                "ptOspite": "29",
+                "stadio": "Stadio Olimpico"
+            },
+            {
+                "giornata": "Quinta Giornata",
+                "casa": "Italia",
+                "ospite": "Galles",
+                "ptCasa": "20",
+                "ptOspite": "61",
+                "stadio": "Stadio Olimpico"
+            },
+            {
+                "giornata": "Quinta Giornata",
+                "casa": "Scozia",
+                "ospite": "Irlanda",
+                "ptCasa": "10",
+                "ptOspite": "40",
+                "stadio": "Murrayfield"
+            },
+            {
+                "giornata": "Quinta Giornata",
+                "casa": "Inghilterra",
+                "ospite": "Francia",
+                "ptCasa": "55",
+                "ptOspite": "35",
+                "stadio": "Twickenham"
+            }];
+ 
+    db.collection('partite', function(err, collection) {
+        collection.insert(partite, {safe:true}, function(err, result) {});
+    });
+
+// Export the DB provider for use in other modules.
+exports.PProvider = PProvider;
--- web.js
+++ web.js
@@ -1,55 +1,28 @@
-var http = require('http'),
-    url = require('url'),
-    path = require('path'),
-    fs = require('fs');
-var mimeTypes = {
-    "html": "text/html",
-    "css": "text/css",
-    "jpeg": "image/jpeg",
-    "jpg": "image/jpeg",
-    "png": "image/png"
-    };
+var express = require('express');
+var app = express();
 
-function processa(req, res) {
-	// parse prende una url come stringa e ritorna un oggetto 
-  var uri = url.parse(req.url).pathname;
-  console.log(req.url);
-  console.log(process.cwd());
-  var filename = path.join(process.cwd(), decodeURI(uri));
-  var statFile;
 
-  try {
-    statFile = fs.lstatSync(filename); // genera un'eccezione se il file non esiste
-  } catch (e) {
-    res.writeHead(404, {'Content-Type': 'text/plain'});
-    res.write('404 Resource Not Found\n');
-    res.end();
-    return;
-  }
+var Provider = require('./router/main.js').PProvider;
 
-	// se la richiesta corrisponde a un file 
-  if (statFile.isFile()) {
-    var mimeType = mimeTypes[path.extname(filename).split(".").reverse()[0]];
-    res.writeHead(200, {'Content-Type': mimeType} );
-	// crea uno stream dal file e lo va a scrivere all'interno di res
-    var fileStream = fs.createReadStream(filename);
-    fileStream.pipe(res);
-  } else if (statFile.isDirectory()) {
-    // se la richiesta corrisponde a un file directory
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('La risorsa '+uri+' è una directory'+'\n');
-    res.end();
-  } else {
-    // se la richiesta punta ad altro oggetto: es. symbolic link
-    res.writeHead(403, {'Content-Type': 'text/plain'});
-    res.write('Request Forbidden \n');
-    res.end();
-  }
+app.configure(function () {
+    app.use(express.logger('dev'));  
+    app.use(express.bodyParser());
+});
 
-}
+// Set up the DB
+var prov = new Provider(process.env.OPENSHIFT_MONGODB_DB_HOST,
+                            parseInt(process.env.OPENSHIFT_MONGODB_DB_PORT),
+                            process.env.OPENSHIFT_MONGODB_DB_USERNAME,
+                            process.env.OPENSHIFT_MONGODB_DB_PASSWORD);
 
+app.get('/partite', prov.findAll);
+app.get('/partite/:id', prov.findById);
+app.post('/partite', prov.addP);
+app.put('/partite/:id', prov.updateP);
+app.delete('/partite/:id', prov.deleteP);
+ 
 var port =  process.env.OPENSHIFT_NODEJS_PORT || 8080;   // Port 8080 if you run locally
 var address =  process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1"; // Listening to localhost if you run locally
 
-var s = http.createServer(processa);
-s.listen(port, address);
+
+var server=app.listen(port, address);
