224d2996f1cb53c4680b04e09eb5ee87932febea
paoloceravolo <paolo.ceravolo@unimi.it>
2015-06-01 08:19:57 +0200

    mongo

--- web.js
+++ web.js
@@ -2,24 +2,25 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
+var games = require('./routes/games');
+var fixtures = require('./fixtures/games');
+var Game = require('./models/Game.js');
 
-var Provider = require('./router/main.js').PProvider;
+var mongoose = require('mongoose');
+mongoose.connect(process.env.OPENSHIFT_MONGODB_DB_URL, function(err) {
+    if(err) {
+        console.log('connection error', err);
+    } else {
+        console.log('connection successful');
+        fixtures.populateDB(); /* POPOLO IL DB SE VUOTO */
+    }
+});
 
-
-//app.use(express.logger('dev'));  
 app.use(bodyParser());
 
-// Set up the DB
-var prov = new Provider(process.env.OPENSHIFT_MONGODB_DB_HOST,
-                            parseInt(process.env.OPENSHIFT_MONGODB_DB_PORT),
-                            process.env.OPENSHIFT_MONGODB_DB_USERNAME,
-                            process.env.OPENSHIFT_MONGODB_DB_PASSWORD);
-
-app.get('/partite', prov.findAll);
-app.get('/partite/:id', prov.findById);
-app.post('/partite', prov.addP);
-app.put('/partite/:id', prov.updateP);
-app.delete('/partite/:id', prov.deleteP);
+// In questo modo posso fare delle mini-app, 
+// attacco il router delle partite sotto la radice /games
+app.use('/games', games);
  
 var port =  process.env.OPENSHIFT_NODEJS_PORT || 8080;   // Port 8080 if you run locally
 var address =  process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1"; // Listening to localhost if you run locally
