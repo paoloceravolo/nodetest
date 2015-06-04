29d45829974a8e753f8302f05a7343c671574a4b
paoloceravolo <paolo.ceravolo@unimi.it>
2015-06-04 14:48:13 +0200

    test

--- web.js
+++ web.js
@@ -1,29 +1,16 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
+var http = require("http");
 
-var games = require('./routes/games');
-var fixtures = require('./fixtures/games');
-var Game = require('./models/Game.js');
+function processa(req, res){
+	var corpo = 'Sono qui! mi hai chimato da ' + req.url + ' con metodo: ' + req.method + '\n';
+	var content_length = corpo.length;
+	
+	res.writeHead(200, {'Content-Length': content_length, 'Content-Type': 'text/plain'});
+	res.end(corpo);
+	
+	}
 
-var mongoose = require('mongoose');
-mongoose.connect(process.env.OPENSHIFT_MONGODB_DB_URL, function(err) {
-    if(err) {
-        console.log('connection error', err);
-    } else {
-        console.log('connection successful');
-        fixtures.populateDB(); /* POPOLO IL DB SE VUOTO */
-    }
-});
-
-app.use(bodyParser.json());
-
-// In questo modo posso fare delle mini-app, 
-// attacco il router delle partite sotto la radice /games
-app.use('/partite', games);
- 
 var port =  process.env.OPENSHIFT_NODEJS_PORT || 8080;   // Port 8080 if you run locally
 var address =  process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1"; // Listening to localhost if you run locally
 
-
-var server=app.listen(port, address);
+var s = http.createServer(processa);
+s.listen(port, address);
